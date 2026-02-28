75261949d7e19bf2f078b59715b21d2235d8051d
Harry Maclean <harry@hmac.dev>
2026-02-28 17:26:02 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Haskell development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ghc
+            cabal-install
+            haskell-language-server
+            ghcid
+          ];
+
+          shellHook = ''
+            echo "Haskell development environment"
+            echo "GHC version: $(ghc --version)"
+            echo "Cabal version: $(cabal --version | head -n1)"
+          '';
+        };
+      }
+    );
+}
